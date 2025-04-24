71d5a815009fc5e66820668d9f043e0deaddb156
chahdhattab <eechahdee74@gmail.com>
2025-04-24 15:31:59 +0100

    "java records"

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -10,17 +10,17 @@ public class Shop {
     public static void main(String[] args) {
         ProductManager pm = new ProductManager() ;
 
-        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.THREE_STAR);
-        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
+        Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
+        /*Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
         Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
         Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
-        Product p5 = p3.applyRating(Rating.THREE_STAR); /* prend meme id, name, price que p3 sans que p3 soit modifé */
+        Product p5 = p3.applyRating(Rating.THREE_STAR); // prend meme id, name, price que p3 sans que p3 soit modifé
         Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR);
         Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
         Product p8 = p4.applyRating(Rating.FIVE_STAR);
-        Product p9 = p1.applyRating(Rating.TWO_STAR);
+        Product p9 = p1.applyRating(Rating.TWO_STAR);*/
 
-
+        /*
         if (p3 instanceof Food f) {
             System.out.println (f.getBestBefore ());
         }
@@ -33,8 +33,9 @@ public class Shop {
         System.out.println(p6.equals(p7));
         System.out.println(p8);
         System.out.println(p9);
-        System.out.println (p3.getBestBefore ());
-
+        System.out.println (p3.getBestBefore ());*/
+        p1 = pm.reviewProduct (p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        pm.printProductReport() ;
     }
 
 
