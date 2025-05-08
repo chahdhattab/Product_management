95e404302d25f486efbacf5de08d9f0496a840d1
chahdhattab <eechahdee74@gmail.com>
2025-05-08 15:10:11 +0100

    "Process array of reviews  Calculate product rating"

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -18,6 +18,7 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -30,7 +31,7 @@ public class ProductManager {
     private DateTimeFormatter dateFormat;
     private NumberFormat moneyFormat;
     private Product product;
-    private Review review;
+    private Review[] reviews = new Review[5];
 
     public ProductManager(Locale locale) {
         this.locale = locale;
@@ -50,8 +51,21 @@ public class ProductManager {
     }
 
     public Product reviewProduct (Product product, Rating rating, String comments) {
-        review = new Review(rating, comments);
-        this.product = product.applyRating(rating);
+        if (reviews [reviews.length-1] != null) { //First, it checks if the last element of the reviews array is not null
+            reviews = Arrays.copyOf (reviews, reviews.length+5); //If the array is full, it expands the array by 5 more slots using Arrays.copyOf()
+        }
+        int sum = 0,i = 0;
+        boolean reviewed = false;
+        while (i < reviews.length && !reviewed){ //When it finds an empty slot, it stores the new review there (with the given rating and comments)
+            if (reviews[i] == null){
+                reviews [i] = new Review (rating, comments);
+                reviewed = true;
+            }
+            sum += reviews[i].rating().ordinal(); //Ratings are assumed to be enums (like ★, ★★, ★★★), so .ordinal() converts them to numbers (e.g., ★ = 1, ★★ = 2, etc.)
+            i++;
+        }
+
+        this.product = product.applyRating(Rateable.convert(Math.round ((float) sum/i))); // The average rating is rounded to the nearest whole number.  The product’s rating is updated using product.applyRating().
         return this.product;
     }
 
@@ -61,13 +75,18 @@ public class ProductManager {
         txt.append (MessageFormat.format (resources.getString ("product"),
                 product.getName() , moneyFormat.format (product.getPrice()), product.getRating().getStars() , dateFormat.format (product. getBestBefore () ), type));
         txt.append ('\n');
-        if (review != null){
-            txt.append (review);
+        for (Review review : reviews){
+            if (review == null){
+                break;
+            }
+            txt.append (MessageFormat.format (resources.getString ("review"),
+                    review.rating ().getStars (), review.comments())) ;
+            txt.append ('\n');
         }
-        else{
-            txt.append (resources.getString ("no.reviews")) ;
+        if(reviews [0] == null){
+            txt.append (resources.getString ("no.reviews"));
+            txt.append ('\n');
         }
-        txt.append ('\n');
         System.out.println (txt);
     }
 }
